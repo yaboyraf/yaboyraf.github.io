a8ca9ed1b3936fc146636be25d1ba08046b29f40
Rafael Guevara <mmmsnacks@gmail.com>
2021-04-28 22:11:58 -0700

    rotating nav project

--- 50projects50days-master/progress-steps/script.js
+++ 50projects50days-master/progress-steps/script.js
@@ -39,7 +39,9 @@ function update() {
 
     if (currentActive === 1) {
         prev.disabled = true
-    } else if (currentActive === circles.length) {
+    } 
+    
+    if (currentActive === circles.length) {
         next.disabled = true
     } else {
         prev.disabled = false
--- 50projects50days-master/rotating-nav-animation/script.js
+++ 50projects50days-master/rotating-nav-animation/script.js
@@ -3,5 +3,4 @@ const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
-
 close.addEventListener('click', () => container.classList.remove('show-nav'))
