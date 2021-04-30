ccb066dc8604c33f995f85e0edead10110adab48
Rafael Guevara <mmmsnacks@gmail.com>
2021-04-29 20:55:10 -0700

    scroll animation project

--- 50projects50days-master/scroll-animation/script.js
+++ 50projects50days-master/scroll-animation/script.js
@@ -5,15 +5,14 @@ window.addEventListener('scroll', checkBoxes)
 checkBoxes()
 
 function checkBoxes() {
-    const triggerBottom = window.innerHeight / 5 * 4
+   const triggerBottom = window.innerHeight / 5 * 4
 
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top
-
-        if(boxTop < triggerBottom) {
-            box.classList.add('show')
-        } else {
-            box.classList.remove('show')
-        }
-    })
+   boxes.forEach(box => {
+       const boxTop = box.getBoundingClientRect().top
+       if(boxTop < triggerBottom) {
+           box.classList.add('show')
+       } else {
+           box.classList.remove('show')
+       }
+   })
 }
