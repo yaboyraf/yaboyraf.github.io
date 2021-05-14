5977ee7752bc0d9238749f3ac3732097622b8d29
Rafael Guevara <mmmsnacks@gmail.com>
2021-05-13 22:44:04 -0700

    dad jokes project with async await

--- 50projects50days-master/dad-jokes/script.js
+++ 50projects50days-master/dad-jokes/script.js
@@ -5,13 +5,28 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
-function generateJoke() {
+// function generateJoke() {
+//   const config =   { 
+//     headers: {
+//     'Accept': 'application/json'
+//   }
+// }
+//   fetch('https://icanhazdadjoke.com', config).then(res => res.json()).then((data) => {
+//     jokeEl.innerHTML = data.joke
+//   })
+// }
+
+//async/await 
+
+async function generateJoke() {
   const config =   { 
     headers: {
-    'Accept': 'application/json'
+      'Accept': 'application/json'
+    }
   }
-}
-  fetch('https://icanhazdadjoke.com', config).then(res => res.json()).then((data) => {
-    jokeEl.innerHTML = data.joke
-  })
+  const response = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await response.json()
+
+  jokeEl.innerHTML = data.joke
 }
