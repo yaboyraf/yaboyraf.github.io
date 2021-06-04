e8754e0ce21619f7f90fc42238ff0b3ae892de59
Rafael Guevara <mmmsnacks@gmail.com>
2021-06-03 22:56:13 -0700

    clock project!

--- 50projects50days-master/theme-clock/script.js
+++ 50projects50days-master/theme-clock/script.js
@@ -5,12 +5,14 @@ const timeEl = document.querySelector('.time')
 const dateEl = document.querySelector('.date')
 const toggle = document.querySelector('.toggle')
 
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", ];
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec", ];
 
 toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html')
-    if (html.classList.contains('dark')) {
+
+    if(html.classList.contains('dark')) {
         html.classList.remove('dark')
         e.target.innerHTML = 'Dark mode'
     } else {
@@ -20,12 +22,12 @@ toggle.addEventListener('click', (e) => {
 })
 
 function setTime() {
-    const time = new Date();
+    const time = new Date()
     const month = time.getMonth()
     const day = time.getDay()
     const date = time.getDate()
     const hours = time.getHours()
-    const hoursForClock = hours >= 13 ? hours % 12 : hours;
+    const hoursForClock = hours % 12
     const minutes = time.getMinutes()
     const seconds = time.getSeconds()
     const ampm = hours >= 12 ? 'PM' : 'AM'
@@ -35,7 +37,8 @@ function setTime() {
     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
 
     timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
-    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class"circle">${date}</span>`
 }
 
 // StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
