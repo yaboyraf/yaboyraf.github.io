0626912a0ca315d65d2877e01e81d8f5695d6e75
Rafael Guevara <mmmsnacks@gmail.com>
2021-06-14 23:27:29 -0700

    content placeholder/loading animation

--- 50projects50days-master/content-placeholder/script.js
+++ 50projects50days-master/content-placeholder/script.js
@@ -5,22 +5,19 @@ const profile_img = document.getElementById('profile_img')
 const name = document.getElementById('name')
 const date = document.getElementById('date')
 
-const animated_bgs = document.querySelectorAll('.animated-bg')
-const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+const animated_bgs = document.querySelectorAll('.aniamted-bg')
+const animated_bgs_texts = document.querySelectorAll('.aniamted-bg-text')
 
 setTimeout(getData, 2500)
 
 function getData() {
-  header.innerHTML =
-    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
-  title.innerHTML = 'Lorem ipsum dolor sit amet'
-  excerpt.innerHTML =
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
-  profile_img.innerHTML =
-    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
-  name.innerHTML = 'John Doe'
-  date.innerHTML = 'Oct 08, 2020'
+    header.innerHTML = '<img src="https://source.unsplash.com/random/" alt="" />'
+    title.innerHTML = 'Lorem ipsum dolor sit amet.'
+    excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, accusamus.'
+    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
+    name.innerHTML = 'John Doe'
+    date.innerHTML = 'Oct 08, 2020'
 
-  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
-  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+    animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
+    animated_bgs_texts.forEach(bg => bg.classList.remove('animated-bg-text'))
 }
