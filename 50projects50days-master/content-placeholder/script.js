b3f17eaebf4d3cec1cc8e69f7f6e354b91082fe4
Rafael Guevara <mmmsnacks@gmail.com>
2021-06-16 21:54:19 -0700

    fix typo on js line 8 and 9

--- 50projects50days-master/content-placeholder/script.js
+++ 50projects50days-master/content-placeholder/script.js
@@ -5,19 +5,19 @@ const profile_img = document.getElementById('profile_img')
 const name = document.getElementById('name')
 const date = document.getElementById('date')
 
-const animated_bgs = document.querySelectorAll('.aniamted-bg')
-const animated_bgs_texts = document.querySelectorAll('.aniamted-bg-text')
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
 
 setTimeout(getData, 2500)
 
 function getData() {
     header.innerHTML = '<img src="https://source.unsplash.com/random/" alt="" />'
     title.innerHTML = 'Lorem ipsum dolor sit amet.'
-    excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, accusamus.'
+    excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum sint iste veritatis nulla, voluptatem repudiandae error fugit beatae odio eius.'
     profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
     name.innerHTML = 'John Doe'
     date.innerHTML = 'Oct 08, 2020'
 
-    animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
-    animated_bgs_texts.forEach(bg => bg.classList.remove('animated-bg-text'))
+    animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+    animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
 }
