d3f24c915041cce09c234f801423f9a8eac6158e
Rafael Guevara <mmmsnacks@gmail.com>
2021-07-08 11:37:05 -0700

    clock fixes

--- 50projects50days-master/theme-clock/script.js
+++ 50projects50days-master/theme-clock/script.js
@@ -33,8 +33,8 @@ function setTime() {
     const ampm = hours >= 12 ? 'PM' : 'AM'
 
     hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
-    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
-    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`
 
     timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds} ${ampm}`
 
