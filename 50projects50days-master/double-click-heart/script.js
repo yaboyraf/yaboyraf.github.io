d8e3cdc4efa566ca0223cb08b9160aeb0432d45d
Rafael Guevara <mmmsnacks@gmail.com>
2021-07-15 22:25:44 -0700

    double click heart effect

--- 50projects50days-master/double-click-heart/script.js
+++ 50projects50days-master/double-click-heart/script.js
@@ -7,10 +7,11 @@ let timesClicked = 0
 loveMe.addEventListener('click', (e) => {
     if(clickTime === 0) {
         clickTime = new Date().getTime()
+        // console.log(clickTime)
     } else {
-        if((new Date().getTime() - clickTime) < 800) {
+        if((new Date().getTime() - clickTime < 800)) {
             createHeart(e)
-            clickTime = 0
+            clickTIme = 0
         } else {
             clickTime = new Date().getTime()
         }
