78ee3aea31d4b5757efa4fcf0bceedc2fef3a91a
yaboyraf <mmmsnacks@gmail.com>
2021-08-18 21:01:34 -0700

    made a todo list with no instructions!

--- 50projects50days-master/notes-app/script.js
+++ 50projects50days-master/notes-app/script.js
@@ -3,17 +3,26 @@ const addBtn = document.getElementById('add')
 const notes = JSON.parse(localStorage.getItem('notes'))
 
 if(notes) {
-    notes.forEach(note => addNewNote(note))
+    let initialId = notes.length - 1;
+    notes.forEach((note, index) => {
+        console.log('@@ initialId is: ', initialId);
+        addNewNote(note, initialId)
+        initialId = initialId - 1;
+    })
 }
 
 addBtn.addEventListener('click', () => addNewNote())
 
-function addNewNote(text = '') {
+function addNewNote(text = '', id = notes.length) {
     const note = document.createElement('div')
-    note.classList.add('note')
+    note.classList.add(`note`)
+    note.classList.add(`note-id-${id}`)
 
     note.innerHTML = `
     <div class="tools">
+        <input type="color" id="notepad-color" name="${id}"
+            value="#e66465">
+        <label for="head">Notepad color</label>
         <button class="edit"><i class="fas fa-edit"></i></button>
         <button class="delete"><i class="fas fa-trash-alt"></i></button>
     </div>
@@ -43,7 +52,6 @@ function addNewNote(text = '') {
     textArea.addEventListener('input', (e) => {
         const { value } = e.target
         main.innerHTML = marked(value)
-
         updateLS()
     })
 
@@ -55,8 +63,14 @@ function updateLS() {
     const notesText = document.querySelectorAll('textarea')
 
     const notes = []
-
     notesText.forEach(note => notes.push(note.value))
 
     localStorage.setItem('notes', JSON.stringify(notes.reverse()))
 }
+
+document.querySelectorAll('#notepad-color').forEach(el => {
+    el.addEventListener('change', (e) => {
+    console.log(parseInt(e.target.name))
+    document.querySelectorAll('.note')[parseInt(e.target.name)].style.backgroundColor = e.target.value
+    })
+  })
--- 50projects50days-master/todo-list/script.js
+++ 50projects50days-master/todo-list/script.js
@@ -1,65 +1,19 @@
 const form = document.getElementById('form')
 const input = document.getElementById('input')
-const todosUL = document.getElementById('todos')
+const submit = document.getElementById('submit')
+const todoList = document.getElementById('todos')
 
-const todos = JSON.parse(localStorage.getItem('todos'))
+const todo = []
 
-if(todos) {
-    todos.forEach(todo => addTodo(todo))
+function addTodo(newTodo) {
+    const li = document.createElement('li')
+    todo.push(newTodo)
+    li.innerHTML = newTodo
+    todoList.appendChild(li)
 }
 
-form.addEventListener('submit', (e) => {
+submit.addEventListener('click', (e) => {
     e.preventDefault()
-
-    addTodo()
+    addTodo(input.value)
+    input.value = ''
 })
-
-function addTodo(todo) {
-    let todoText = input.value
-
-    if(todo) {
-        todoText = todo.text
-    }
-
-    if(todoText) {
-        const todoEl = document.createElement('li')
-        if(todo && todo.completed) {
-            todoEl.classList.add('completed')
-        }
-
-        todoEl.innerText = todoText
-
-        todoEl.addEventListener('click', () => {
-            todoEl.classList.toggle('completed')
-            updateLS()
-        }) 
-
-        todoEl.addEventListener('contextmenu', (e) => {
-            e.preventDefault()
-
-            todoEl.remove()
-            updateLS()
-        }) 
-
-        todosUL.appendChild(todoEl)
-
-        input.value = ''
-
-        updateLS()
-    }
-}
-
-function updateLS() {
-    todosEl = document.querySelectorAll('li')
-
-    const todos = []
-
-    todosEl.forEach(todoEl => {
-        todos.push({
-            text: todoEl.innerText,
-            completed: todoEl.classList.contains('completed')
-        })
-    })
-
-    localStorage.setItem('todos', JSON.stringify(todos))
-}
