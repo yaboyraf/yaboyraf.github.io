bc99997b76941192631af62021ddd1e2ecb8afe8
yaboyraf <mmmsnacks@gmail.com>
2021-09-01 21:03:42 -0700

    broken

--- react-course-projects/indecision-app/public/scripts/app.js
+++ react-course-projects/indecision-app/public/scripts/app.js
@@ -1,12 +1,79 @@
-"use strict";
+'use strict';
 
-console.log("@@ app.js is running");
+//console.log("@@ app.js is running")
 
-// JSX means: Javascript XML
-var template = React.createElement(
-  "p",
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Let the computer decide',
+  options: ['One', 'Two']
+
+  // JSX means: Javascript XML
+};var template = React.createElement(
+  'div',
   null,
-  "change"
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    'app.subtitle'
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options:' : 'No Options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'item 1'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'item 2'
+    )
+  )
+);
+
+var user = {
+  name: 'Raf',
+  age: 28,
+  location: 'San Jose'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
 );
 var appRoot = document.getElementById('app');
 
--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,7 +1,43 @@
-console.log("@@ app.js is running")
+//console.log("@@ app.js is running")
+
+let app = {
+  title: 'Indecision App',
+  subtitle: 'Let the computer decide',
+  options: ['One', 'Two']
+}
 
 // JSX means: Javascript XML
-var template = <p>change</p>;
+var template = (
+    <div>
+        <h1>{app.title}</h1> 
+        {app.subtitle && <p>app.subtitle</p>}
+        <p>{app.options.length > 0 ? 'Here are your options:' : 'No Options'}</p>
+        <ol>
+          <li>item 1</li>
+          <li>item 2</li>
+        </ol>
+    </div>
+)
+
+var user = {
+  name: 'Raf',
+  age: 28,
+  location: 'San Jose'
+}
+
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>
+  }
+}
+
+var templateTwo = (
+   <div>
+      <h1>{user.name ? user.name : 'Anonymous'}</h1>
+      {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+      {getLocation(user.location)}
+   </div>
+)
 var appRoot = document.getElementById('app')
 
 ReactDOM.render(template, appRoot)
